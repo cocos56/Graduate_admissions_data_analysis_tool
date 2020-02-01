8e80e09154c7dc107176612dfaeb380ecd892af0
coco <zj175@139.com>
2020-02-01 15:24:48 +0800

    更新于2020年02月01日	15:24:48.57

--- V3_0/Storer/Redis/redis.py
+++ V3_0/Storer/Redis/redis.py
@@ -1,4 +1,5 @@
 from redis import StrictRedis
+import redis
 
 sr = StrictRedis(host='st.coco56.top', port=6379, db=0)
 
@@ -9,7 +10,11 @@ def getInt(key, default=0):
 	:param default:当键不存在时设置的默认值
 	:return: 键所对应的值
 	"""
-	value = int(sr.get(key))
+	try:
+		value = int(sr.get(key))
+	except redis.exceptions.ConnectionError as err:
+		print(err)
+		value = getInt(key, default)
 	if value is None:
 		value = default
 		setInt(key, value)
